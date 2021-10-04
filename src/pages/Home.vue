9ed4f7be724b1c071aaf35e4f9e8dd71bfa8a62e
Filipp Gareev <filok94@gmail.com>
2021-10-05 00:34:29 +0300

    add animation to auth_window

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,15 +3,26 @@
         <div @click="activeWindow=true" :class="{active:activeWindow}">Авторизация</div>
         <div  @click="activeWindow=false" :class="{active:!activeWindow}">Регистрация</div>
     </div>
-    <Auth v-if="activeWindow"></Auth>
-    <Reg v-else></Reg>
+    <transition :name="nameOfTransition" mode="out-in">
+        <Auth v-if="activeWindow"></Auth>
+        <Reg v-else></Reg>
+    </transition> 
 </template>
 
 <script setup>
 import Auth from '../components/AuthWindow.vue'
 import Reg from '../components/RegWindow.vue'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 const activeWindow = ref(true)
+
+//watch on which window is active and change the animation
+let nameOfTransition = ref('reg')
+watch(
+    ()=>activeWindow.value,(newValue, oldValue)=>{
+        if (newValue){
+            nameOfTransition.value = 'auth'
+        } else nameOfTransition.value = 'reg'
+    })
 </script>
 
 <style lang="scss">
@@ -41,5 +52,29 @@ const activeWindow = ref(true)
     .home-navigation {
     flex-wrap: nowrap;
     }
+}
+
+.auth-enter-active,
+.auth-leave-active,
+.reg-enter-active,
+.reg-leave-active {
+  transition: all .3s ease;
+}
+.auth-enter-from,
+.reg-leave-to {
+    opacity: 0;
+    transform: translate(300px, -10px);
+}
+.auth-leave-to,
+.reg-enter-from {
+  opacity: 0;
+  transform: translate(-300px, -10px);
+}
+.auth-enter-to,
+.auth-leave-from,
+.reg-enter-to,
+.reg-leave-from {
+    opacity: 1;
+    transform: translateX(0, 0);
 }
 </style>
